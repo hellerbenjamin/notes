dd820ab80a43289db6be513e5bbe8966bb703863
Benjamin Heller <ben@benjaminheller.net>
2021-11-06 15:24:05 -0800

    Docs: Docblocks

--- app/Repositories/ModelRepository.php
+++ app/Repositories/ModelRepository.php
@@ -5,28 +5,58 @@ namespace App\Repositories;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
+/**
+ *
+ */
 abstract class ModelRepository
 {
 
+    /**
+     * @param  Model  $model
+     */
     public function __construct(protected Model $model)
     {
     }
 
+    /**
+     * @param $id
+     *
+     * @return Model|null
+     */
     public function find($id): ?Model
     {
         return $this->model->find($id);
     }
 
+    /**
+     * @param  string  $column
+     * @param  null  $operator
+     * @param  null  $value
+     * @param  string  $boolean
+     *
+     * @return Collection
+     */
     public function findBy(string $column, $operator = null, $value = null, $boolean = 'and'): Collection
     {
         return $this->model->where($column, $operator, $value, $boolean)->get();
     }
 
+    /**
+     * @param  array  $data
+     *
+     * @return Model
+     */
     public function create(array $data): Model
     {
         return $this->model->create($data);
     }
 
+    /**
+     * @param  int  $id
+     * @param  array  $data
+     *
+     * @return Model
+     */
     public function update(int $id, array $data): Model
     {
         $model = $this->model->find($id);
@@ -35,6 +65,11 @@ abstract class ModelRepository
         return $model;
     }
 
+    /**
+     * @param  int  $id
+     *
+     * @return bool
+     */
     public function delete(int $id): bool
     {
         return $this->find($id)->delete();
--- app/Repositories/ModelRepositoryInterface.php
+++ app/Repositories/ModelRepositoryInterface.php
@@ -5,15 +5,47 @@ namespace App\Repositories;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
+/**
+ *
+ */
 interface ModelRepositoryInterface
 {
+    /**
+     * @param $id
+     *
+     * @return Model|null
+     */
     public function find($id): ?Model;
 
+    /**
+     * @param  string  $column
+     * @param  null  $operator
+     * @param  null  $value
+     * @param  string  $boolean
+     *
+     * @return Collection
+     */
     public function findBy(string $column, $operator = null, $value = null, $boolean = 'and'): Collection;
 
+    /**
+     * @param  array  $data
+     *
+     * @return Model
+     */
     public function create(array $data): Model;
 
+    /**
+     * @param  int  $id
+     * @param  array  $data
+     *
+     * @return Model
+     */
     public function update(int $id, array $data): Model;
 
+    /**
+     * @param  int  $id
+     *
+     * @return bool
+     */
     public function delete(int $id): bool;
 }
